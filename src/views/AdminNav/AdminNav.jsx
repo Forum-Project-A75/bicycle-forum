fddd38a8d05e101cf9a93316eb710bec3390751c
KeplerFonx <momchilthesoldier@gmail.com>
2026-02-22 21:36:27 +0200

    add minor styling changes

--- src/views/AdminNav/AdminNav.jsx
+++ src/views/AdminNav/AdminNav.jsx
@@ -3,11 +3,46 @@ import { NavLink } from 'react-router-dom';
 export default function AdminNav() {
   return (
     <>
-      <NavLink to="/" className={({isActive}) => isActive ? "active" : ""}>Home</NavLink>
-      <NavLink to="/posts" className={({isActive}) => isActive ? "active" : ""}>All Posts</NavLink>
-      <NavLink to="/tags" className={({isActive}) => isActive ? "active" : ""}>Tags</NavLink>
-      <NavLink to="/admin/users" className={({isActive}) => isActive ? "active" : ""}>Users</NavLink>
-      <NavLink to="/admin/posts" className={({isActive}) => isActive ? "active" : ""}>Moderation</NavLink>
+      <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+        Home
+      </NavLink>
+      <NavLink
+        to="/posts"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        All Posts
+      </NavLink>
+      <NavLink
+        to="/create"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        Create Post
+      </NavLink>
+      <NavLink
+        to="/tags"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        Tags
+      </NavLink>
+
+      <NavLink
+        to="/admin/users"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        Users
+      </NavLink>
+      <NavLink
+        to="/admin/posts"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        Moderation
+      </NavLink>
+      <NavLink
+        to="/profile"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        Profile
+      </NavLink>
     </>
   );
 }

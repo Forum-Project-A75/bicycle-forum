354f2390b0829b550ee80c12b2b506ab5c012331
KeplerFonx <momchilthesoldier@gmail.com>
2026-02-23 19:24:44 +0200

    update post styling

--- src/components/CommentNode/CommentNode.jsx
+++ src/components/CommentNode/CommentNode.jsx
@@ -10,31 +10,34 @@ export default function CommentNode({ comment }) {
 
   return (
     <div className="comment">
-      <div>{comment.handle}</div>
-      {
-        <VotePanel
-          postId={comment.id}
-          score={comment.score}
-          upvotes={comment.upvotes}
-          downvotes={comment.downvotes}
-          userVote={comment.my_vote}
-        />
-      }
-      <div>
-        <div className="comment-content">{comment.content}</div>
-        <button className="reply-button" onClick={() => setReplying(true)}>
-          Reply
-        </button>
-
-        {replying && (
-          <CommentsCreator
-            userId={user.id}
-            parentId={comment.id}
-            setReplying={setReplying}
+      <div className="comment-details">
+        <div>{comment.handle}</div>
+        {
+          <VotePanel
+            postId={comment.id}
+            score={comment.score}
+            upvotes={comment.upvotes}
+            downvotes={comment.downvotes}
+            userVote={comment.my_vote}
           />
-        )}
-      </div>
+        }
+        <div>
+          <div className="comment-content">{comment.content}</div>
+          <div className="reply-container">
+            <button className="reply-button" onClick={() => setReplying(true)}>
+              Reply
+            </button>
 
+            {replying && (
+              <CommentsCreator
+                userId={user.id}
+                parentId={comment.id}
+                setReplying={setReplying}
+              />
+            )}
+          </div>
+        </div>
+      </div>
       {/* ДЕЦАТА */}
       {comment.children.map((child) => (
         <CommentNode key={child.id} comment={child} />

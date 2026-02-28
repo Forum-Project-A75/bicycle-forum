102a865aa8c11732b00c2feaf5bd93a014255c53
KeplerFonx <momchilthesoldier@gmail.com>
2026-02-28 19:51:57 +0200

    add more styling to profile component

--- src/views/UserProfile/UserProfile.jsx
+++ src/views/UserProfile/UserProfile.jsx
@@ -1,13 +1,16 @@
 import './UserProfile.css';
 import { useAuth } from '../../hoc/auth-context';
 import uploadAvatar from '../../Services/db.services/setAvatar';
-import { getUserByUid, updateAvatar, updateUserNames } from '../../Services/db.services/user.services.js';
+import {
+  getUserByUid,
+  updateAvatar,
+  updateUserNames,
+} from '../../Services/db.services/user.services.js';
 import { getAvatarUrl } from '../../Services/user.services/user.service.js';
 import { createLogger, LOG_MODULES } from '../../debug/debug.js';
 import { useState, useEffect } from 'react';
 import { checkInputUserData } from './validate.data.js';
 import ShowUserPosts from '../../components/ShowUserPosts/ShowUserPosts.jsx';
-
 
 const log = createLogger(LOG_MODULES.USER_PROFILE);
 
@@ -22,7 +25,7 @@ export default function UserProfile() {
   const [registrationErrors, setRegistrationErrors] = useState({});
   //const defaultAvatar = '../../../images/default.png';
 
-  const [activeTab, setActiveTab] = useState("profile");
+  const [activeTab, setActiveTab] = useState('profile');
 
   const setImage = (event) => {
     const file = event.target.files[0];
@@ -66,23 +69,22 @@ export default function UserProfile() {
 
     try {
       await uploadAvatar(user.id, avatar);
-      
+
       let avatar_url = getAvatarUrl(user.id);
       setPreview(avatar_url);
       updateAvatar(avatar_url);
       alert('Avatar uploaded!');
-    } 
-    catch (err) {
+    } catch (err) {
       log.error('uploadAvatar: ', err.message, err);
       alert('Upload failed');
     }
   };
 
   const handleSave = async () => {
-    log.log("handleSave: ", updatedState);
+    log.log('handleSave: ', updatedState);
     const validationErrors = checkInputUserData(updatedState);
-    log.log("validationErrors: ", validationErrors);
-    
+    log.log('validationErrors: ', validationErrors);
+
     if (Object.keys(validationErrors).length !== 0) {
       setRegistrationErrors(validationErrors);
       return;
@@ -90,13 +92,16 @@ export default function UserProfile() {
 
     try {
       await updateUserNames(updatedState.firstName, updatedState.lastName);
-    }
-    catch (err) {
+    } catch (err) {
       log.error(err);
       return;
     }
 
-    setUserData({...userData, firstName: updatedState.firstName, lastName: updatedState.lastName });
+    setUserData({
+      ...userData,
+      firstName: updatedState.firstName,
+      lastName: updatedState.lastName,
+    });
   };
 
   const updateNames = (prop) => (e) => {
@@ -108,171 +113,86 @@ export default function UserProfile() {
 
   return (
     <div id="user-profile">
+      <div className="tabs">
+        <button
+          className={activeTab === 'profile' ? 'active' : ''}
+          onClick={() => setActiveTab('profile')}
+        >
+          Edit Profile
+        </button>
 
+        <button
+          className={activeTab === 'posts' ? 'active' : ''}
+          onClick={() => setActiveTab('posts')}
+        >
+          My Posts
+        </button>
 
-    <div className="tabs">
-  <button
-    className={activeTab === "profile" ? "active" : ""}
-    onClick={() => setActiveTab("profile")}
-  >
-    Edit Profile
-  </button>
-
-  <button
-    className={activeTab === "posts" ? "active" : ""}
-    onClick={() => setActiveTab("posts")}
-  >
-    My Posts
-  </button>
-
-  <button
-    className={activeTab === "comments" ? "active" : ""}
-    onClick={() => setActiveTab("comments")}
-  >
-    My Comments
-  </button>
-</div> 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <button
+          className={activeTab === 'comments' ? 'active' : ''}
+          onClick={() => setActiveTab('comments')}
+        >
+          My Comments
+        </button>
+      </div>
       <p>Edit Details</p>
       <hr />
 
-      {activeTab === "posts" && (
-  <ShowUserPosts />
-)}
-
-
-      {activeTab === "profile" && (<div id="edit-section">
-        <div className="field" id="avatar-field">
-          <img src={preview || dbAvatar} alt="avatar" />
-          <div>
-            <input type="file" accept="image/*" onChange={setImage} />
-            <button onClick={handleUpload} disabled={!avatar}>
-              Upload
-            </button>
+      {activeTab === 'posts' && <ShowUserPosts />}
+      {activeTab === 'profile' && (
+        <div id="edit-section">
+          <div className="field" id="avatar-field">
+            <img src={preview || dbAvatar} alt="avatar" />
+            <div>
+              <input type="file" accept="image/*" onChange={setImage} />
+              <button onClick={handleUpload} disabled={!avatar}>
+                Upload
+              </button>
+            </div>
+          </div>
+          <div id="names">
+            <div className="email">
+              <p>Email:</p>
+              <p>{userData?.email || ''}</p>
+            </div>
+            <div className="handle">
+              <p>Handle: </p>
+              <p>{userData?.handle || ''}</p>
+            </div>
+            <div className="field">
+              <label htmlFor="First Name">First Name: </label>
+              <div className="input-layout">
+                <input
+                  value={(updatedState && updatedState.firstName) || ''}
+                  onChange={updateNames('firstName')}
+                  type="text"
+                  placeholder="Enter First Name"
+                  id="firstName"
+                />
+                {registrationErrors.firstName && (
+                  <p className="error">{registrationErrors.firstName}</p>
+                )}
+              </div>
+            </div>
+            <div className="field">
+              <label htmlFor="Last Name">Last Name: </label>
+              <div className="input-layout">
+                <input
+                  value={(updatedState && updatedState.lastName) || ''}
+                  onChange={updateNames('lastName')}
+                  type="text"
+                  placeholder="Enter Last Name"
+                  id="lastName"
+                />
+                {registrationErrors.lastName && (
+                  <p className="error">{registrationErrors.lastName}</p>
+                )}
+              </div>
+            </div>
+            <button onClick={handleSave}>Save Changes</button>
           </div>
         </div>
-        <div id="names">
-          <div className="email">
-            <label htmlFor="Email">Email: </label>
-            <div className="input-layout">
-              <input
-                value={userData && userData.email || ''}
-                type="text"
-                readOnly
-              />
-            </div>
-          </div>
-          <div className="handle">
-            <label htmlFor="Handle">User name: </label>
-            <div className="input-layout">
-              <input
-                value={userData && userData.handle || ''}
-                type="text"
-                readOnly
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label htmlFor="First Name">First Name: </label>
-            <div className="input-layout">
-              <input
-                value={(updatedState && updatedState.firstName) || ''}
-                onChange={updateNames('firstName')}
-                type="text"
-                placeholder="Enter First Name"
-                id="firstName"
-              />
-              {registrationErrors.firstName && (
-              <p className="error">{registrationErrors.firstName}</p>
-               )}
-            </div>
-          </div>
-          <div className="field">
-            <label htmlFor="Last Name">Last Name: </label>
-            <div className="input-layout">
-              <input
-                value={(updatedState && updatedState.lastName) || ''}
-                onChange={updateNames('lastName')}
-                type="text"
-                placeholder="Enter Last Name"
-                id="lastName"
-              />
-              {registrationErrors.lastName && (
-              <p className="error">{registrationErrors.lastName}</p>
-               )}
-            </div>
-          </div>
-          <button onClick={handleSave}>
-              Save Changes
-          </button>
-        </div>
-      </div>)}
-
-   
-      
+      )}
     </div>
-              
   );
-
-
-
-
-
-
-
-
-
-
-
 }
-
-

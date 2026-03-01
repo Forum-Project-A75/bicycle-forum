26bb0e47ed94e7ffab5c8dd41bc2464408ad7403
KeplerFonx <momchilthesoldier@gmail.com>
2026-03-01 19:14:06 +0200

    add tags styling

--- src/views/Tags/Tags.jsx
+++ src/views/Tags/Tags.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 //import { supabase } from "../supabaseClient";
 //import PostCard from "./PostCard";
-import PostCard from "../PostCard/PostCard";
-import { getAllTags } from "../../Services/db.services/tags.services";
-import { createLogger, LOG_MODULES } from "../../debug/debug";
-import { getPostsFiltereByTag } from "../../Services/posts.services/post.services";
-import { getCommentsFiltered, getPostStats } from "../../Services/posts.services/post.services";
+import PostCard from '../PostCard/PostCard';
+import { getAllTags } from '../../Services/db.services/tags.services';
+import { createLogger, LOG_MODULES } from '../../debug/debug';
+import { getPostsFiltereByTag } from '../../Services/posts.services/post.services';
+import {
+  getCommentsFiltered,
+  getPostStats,
+} from '../../Services/posts.services/post.services';
+import './Tags.css';
 
 const log = createLogger(LOG_MODULES.TAGS);
 
 export default function PostsTags() {
-
   const [tags, setTags] = useState([]);
   const [posts, setPosts] = useState([]);
   const [activeTag, setActiveTag] = useState(null);
@@ -20,11 +23,10 @@ export default function PostsTags() {
     const fetchTags = async () => {
       try {
         const tags = await getAllTags();
-        log.log("fetchTags: tags: ", tags);
+        log.log('fetchTags: tags: ', tags);
         setTags(tags);
-      }
-      catch(err) {
-        log.error("fetchTags: ", err.message, err);
+      } catch (err) {
+        log.error('fetchTags: ', err.message, err);
       }
     };
 
@@ -34,56 +36,51 @@ export default function PostsTags() {
   const handleTagClick = async (tagId) => {
     setActiveTag(tagId);
 
-    
     try {
-        const tagedPostsData = await getPostsFiltereByTag(tagId);
+      const tagedPostsData = await getPostsFiltereByTag(tagId);
 
+      const enrichedPosts = await Promise.all(
+        tagedPostsData.map(async (post) => {
+          //const data = await getPostDirectComments(post.id);
+          //const data = await getCommentsFiltered(post.id);
+          const data = await getPostStats(post.id);
+          return {
+            ...post,
+            comment_count: data[0]?.comment_count ?? 0,
+            upvotes: data[0]?.upvotes ?? 0,
+            downvotes: data[0]?.downvotes ?? 0,
+          };
+        }),
+      );
 
-         const enrichedPosts = await Promise.all(
-                  tagedPostsData.map(async (post) => {
-                    //const data = await getPostDirectComments(post.id);
-                    //const data = await getCommentsFiltered(post.id);
-                    const data = await getPostStats(post.id);
-                    return {
-                      ...post,
-                      comment_count: data[0]?.comment_count ?? 0,
-                      upvotes: data[0]?.upvotes ?? 0,
-                      downvotes: data[0]?.downvotes ?? 0,
-                    };
-                  })
-                );
-
-        setPosts(enrichedPosts);
-    }
-    catch(err) {
-        log.error("handleTagClick: ", err);
+      setPosts(enrichedPosts);
+    } catch (err) {
+      log.error('handleTagClick: ', err);
     }
   };
 
   return (
-    <div>
-
+    <div id="posts-tags">
       {/* TAG BUTTONS */}
-      <div style={{ marginBottom: "20px" }}>
-        {tags.map(tag => (
+      <div id="tags-container">
+        {tags.map((tag) => (
           <button
             key={tag.id}
+            className="tag-button"
             onClick={() => handleTagClick(tag.id)}
-            style={{
-              marginRight: "10px",
-              background: activeTag === tag.id ? "#ccc" : "#fff"
-            }}
           >
             {tag.name}
           </button>
         ))}
       </div>
 
-      {/* POSTS */}
-      {posts.map(post => (
-        <PostCard key={post.id} post={post} />
-      ))}
-
+      <div id="posts-from-tags">
+        {/* POSTS */}
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+        {!activeTag && <p>Select a tag to search for posts that include it.</p>}
+      </div>
     </div>
   );
 }
